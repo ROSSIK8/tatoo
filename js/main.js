4f232fc8affeb99da7aae279a8b282b3d7ef9d91
Sanzhay Ayurov <ayurov2004@gmail.com>
2024-05-17 14:51:00 +0900

    adding slider functionality

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function hideNav(e) {
 
 
 // -------------- ABOUT-US  -------------- 
-// динамическое выевление высоты кнопки слайдеров
+// динамическое выявление высоты кнопки слайдеров
 function updateArrowHeight() {
     const arrows = document.querySelectorAll('.arrow');
     Array.from(arrows).forEach((item) => {
@@ -75,3 +75,142 @@ function openCloseText(e) {
         child.classList.toggle('hide')
     }
 }
+
+// функционал слайдера
+
+let currentIndexAddress = 0;
+let currentIndexMyWorks = 0;
+const totalSlidesAdress = document.querySelectorAll('.address__img').length;
+const totalSlidesMyMorks = document.querySelectorAll('.my-works__item').length;
+
+function showSlide(slides_, totalSlides, currentIndex) {
+    const slides = document.querySelector(slides_);
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+// функции смены слайдов для address__slider
+const nextSlideAddres = document.querySelector('.address__btn--right');
+const prevSlideAddres = document.querySelector('.address__btn--left');
+nextSlideAddres.addEventListener('click', nextSlideForAddress)
+prevSlideAddres.addEventListener('click', prevSlideForAddress)
+
+function nextSlideForAddress() {
+    currentIndexAddress++
+    if (currentIndexAddress > totalSlidesAdress - 1) {
+        currentIndexAddress = totalSlidesAdress - 1;
+    }
+    showSlide('.address__imgs', totalSlidesAdress, currentIndexAddress);
+}
+
+function prevSlideForAddress() {
+    currentIndexAddress--
+    if (currentIndexAddress < 0) {
+        currentIndexAddress = 0;
+    }
+    showSlide('.address__imgs', totalSlidesAdress, currentIndexAddress);
+}
+
+// функции смены слайдов для my-works__slider
+const nextSlideMyWorks = document.querySelector('.my-works__btn--right');
+const prevSlideMyWorks = document.querySelector('.my-works__btn--left');
+nextSlideMyWorks.addEventListener('click', nextSlideForMyWorks)
+prevSlideMyWorks.addEventListener('click', prevSlideForMyWorks)
+
+function nextSlideForMyWorks() {
+    currentIndexMyWorks++
+    if (currentIndexMyWorks > totalSlidesMyMorks - 1) {
+        currentIndexMyWorks = totalSlidesMyMorks - 1;
+    }
+    showSlide('.my-works__imgs', totalSlidesMyMorks, currentIndexMyWorks);
+}
+
+function prevSlideForMyWorks() {
+    currentIndexMyWorks--
+    if (currentIndexMyWorks < 0) {
+        currentIndexMyWorks = 0;
+    }
+    showSlide('.my-works__imgs', totalSlidesMyMorks, currentIndexMyWorks);
+}
+
+
+
+
+
+// let currentIndexAddress = 0;
+// let currentIndexMyWorks = 0;
+// const totalSlidesAdress = document.querySelectorAll('.address__img').length;
+// const totalSlidesMyMorks = document.querySelectorAll('.my-works__img').length;
+
+// function showSlide(index, slides_, currentIndex) {
+//     const slides = document.querySelector(slides_);
+//     console.log(totalSlides);
+//     console.log(index);
+
+//     currentIndex = index;
+//     console.log(currentIndex);
+//     console.log('--------------------------------');
+
+//     const offset = -currentIndex * 100;
+//     slides.style.transform = `translateX(${offset}%)`;
+// }
+
+// // функции смены слайдов для address__slider
+// const nextSlideAddres = document.querySelector('.address__btn--right');
+// const prevSlideAddres = document.querySelector('.address__btn--left');
+// nextSlideAddres.addEventListener('click', nextSlideForAddress)
+// prevSlideAddres.addEventListener('click', prevSlideForAddress)
+
+// function nextSlideForAddress() {
+//     currentIndexAddress++
+//     if (index > (totalSlides - 1)) {
+//         index = totalSlides - 1;
+//         currentIndex = totalSlides - 1;
+//     } else if (index < 0) {
+//         index = 0;
+//         currentIndex = 0;
+//     }
+//     showSlide(currentIndexAddress, '.address__imgs', currentIndexAddress);
+// }
+
+// function prevSlideForAddress() {
+//     currentIndexAddress--
+//     if (index > (totalSlides - 1)) {
+//         index = totalSlides - 1;
+//         currentIndex = totalSlides - 1;
+//     } else if (index < 0) {
+//         index = 0;
+//         currentIndex = 0;
+//     }
+//     showSlide(currentIndexAddress, '.address__imgs', currentIndexAddress);
+// }
+
+// // функции смены слайдов для my-works__slider
+// const nextSlideMyWorks = document.querySelector('.my-works__btn--right');
+// const prevSlideMyWorks = document.querySelector('.my-works__btn--left');
+// nextSlideMyWorks.addEventListener('click', nextSlideForMyWorks)
+// prevSlideMyWorks.addEventListener('click', prevSlideForMyWorks)
+
+// function nextSlideForMyWorks() {
+//     currentIndexMyWorks++
+//     if (index > (totalSlides - 1)) {
+//         index = totalSlides - 1;
+//         currentIndex = totalSlides - 1;
+//     } else if (index < 0) {
+//         index = 0;
+//         currentIndex = 0;
+//     }
+//     showSlide(currentIndexMyWorks, '.my-works__imgs', '.my-works__item', currentIndexMyWorks);
+// }
+
+// function prevSlideForMyWorks() {
+//     currentIndexMyWorks--
+//     if (index > (totalSlides - 1)) {
+//         index = totalSlides - 1;
+//         currentIndex = totalSlides - 1;
+//     } else if (index < 0) {
+//         index = 0;
+//         currentIndex = 0;
+//     }
+//     showSlide(currentIndexMyWorks, '.my-works__imgs', '.my-works__item', currentIndexMyWorks);
+// }
